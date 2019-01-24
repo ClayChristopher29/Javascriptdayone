0fa1567a5dd88cf17a48fe2e2d8a7b851847efe8
Ryan Clay <clay.Christopher29@yahoo.com>
2019-01-24 10:45:44 -0500

    dayonepractice

---
+++ java.js
@@ -0,0 +1,13 @@
+
+// Use JavaScript to obtain a reference to all article__header elements and change their classList property value to article__header important.
+// Obtain a reference the element with a class of dashed and remove it.
+// Obtain a reference the element with a class of article_footer and add the class of goldenrod it.
+document.querySelector(".article__header").textContent = ("Welcome to Ryan's Blog")
+
+document.querySelector(".article__header")
+.classList.add("_important")
+ 
+document.querySelector(".article__footer").classList.add("goldenrod")
+
+
+
